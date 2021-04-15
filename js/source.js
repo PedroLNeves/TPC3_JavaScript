ff7bec46477dfac2a4cfe9f28c82d6b384f34fcf
Pedro Neves <d4rk.nth3g4m3rx@gmail.com>
2021-04-15 22:38:28 +0100

    TPCJS
    TPC 3, sobre java script acabado

---
+++ js/source.js
@@ -0,0 +1,34 @@
+let writeOutput = function(text)
+{
+    document.write(`<h1>${text}</h1>`)
+}
+
+let truncateString = function(str, num)
+{
+    if (str.length > num)
+    {
+        return str.slice(0, num) + "...";
+    }
+     else {
+        return str;
+    }
+}
+writeOutput(truncateString("Hoje está um lindo dia", 12));
+
+//truncateString("Hoje está um lindo dia", 22) -> "Hoje está um lindo dia"
+//truncateString("Hoje ésta um lindo dia", 8) -> "Hoje est...";
+
+let titleCase = function(str)
+{   
+   return str.replace(/\w\S*/g, function(txt)
+   {
+       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+   } );
+}
+
+titleCase("Hoje está um lindo dia, e de facto os alunos estão muito calados!")
+
+writeOutput(titleCase("Hoje está um lindo dia, e de facto os alunos estão muito calados!"));
+
+
+//titleCase("Hoje está um lindo dia") -> "Hoje Está Um Lindo Dia"
